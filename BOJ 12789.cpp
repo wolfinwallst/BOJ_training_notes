3af937a42bc0ad2563b376fdd2b186edda8d7888
wolfinwallst <128706962+wolfinwallst@users.noreply.github.com>
2025-01-27 22:11:51 +0900

    #12789 도키도키 간식드리미_스택

---
+++ BOJ 12789.cpp
@@ -0,0 +1,27 @@
+#include <iostream> // #12789 도키도키 간식드리미_스택
+#include <stack>
+using namespace std;
+
+int main() {
+    int N, order = 1;
+    cin >> N;
+    stack<int> stk;
+
+    for(int i=0; i<N; i++) {
+        int x;
+        cin >> x;
+        
+        if(x == order) order++; // 순서가 맞다면 다음 순서로
+        else stk.push(x);       // 안 맞으면 스택에 쌓고
+
+        while(!stk.empty() && stk.top()==order) { // 스택 맨 위가 현재 순서이면
+            stk.pop();
+            order++;            // 다음 순서로
+        }
+    }
+
+    if(stk.empty()) cout << "Nice";
+    else cout << "Sad";
+
+    return 0;
+}
