d2532d200f6fbe04ee1b7cef65f9ade47abdfd27
wolfinwallst <128706962+wolfinwallst@users.noreply.github.com>
2025-02-02 13:14:37 +0900

    # 2739 구구단_반복문

---
+++ BOJ 2793.py
@@ -0,0 +1,12 @@
+# 2739 구구단_반복문
+n = int(input())
+
+for i in range(1, 10):
+    print(n,'*', i , '=', n*i)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# 2884 알람 시계_조건문
+n = int(input())
+
+for i in range(1, 10):
+    print("%d * %d = %d" % (n, i, n * i))
